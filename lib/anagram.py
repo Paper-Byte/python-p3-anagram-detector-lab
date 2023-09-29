2dc0d20f9e3e1bc16838715208f2cc804881fc1e
Noah <noahhughesph@gmail.com>
2023-09-29 16:17:41 -0400

    all tests passing

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word_to_check):
+        self.word_to_check = word_to_check
+
+    def match(self, list_of_words):
+        anagrams_found = []
+        for word in list_of_words:
+            if ("".join(sorted(self.word_to_check)) == "".join(sorted(word))):
+                anagrams_found.append(word)
+        return anagrams_found if len(anagrams_found) > 0 else []
